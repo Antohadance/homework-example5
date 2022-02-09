45f3776804c2e0d55049064e09b7e7e99e1a8165
Semec <Semechkov98@gmail.com>
2022-02-09 10:35:31 +0300

    ДЗ Сдано, пересдача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,31 +16,27 @@ public class Main {
             System.out.println(weights[i]);
         }
         //3.
-        int [] month = new int [12];
-        month [0] = 30;
-        float novemberMonth = month[11];
-        System.out.println(novemberMonth);
-        System.out.println(month[0]);
-        System.out.println(month[11]);
-        int november = 11;
-        System.out.println(month[november]);
-        for (int i = 11; i < month.length; i++) {
-            System.out.println(month[i]);
+        float [] weigth = {1.233f, 2.33f, 4.111f,33f};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(weigth[i]);
         }
         //Задание №2
-        float [] weigh = {1,2,3,1.57f,7.654f,9.986f,0,30,0,0,0};
-        System.out.println(weigh[0]+ " , " + weigh[1]+ " , " + weigh[2]);
-        System.out.println(weigh[3]+ " , " + weigh[4]+ " , " + weigh[5]);
-        System.out.println(weigh[6]+ " , " + weigh[7]+ " , " + weigh[8] + " , " + weigh[9]+ " , " + weigh[10]);
+        float [] weigths = {1,2,3,1.57f,7.654f,9.986f,1.233f, 2.33f, 4.111f,33f};
+        for (int i = 0; i < 1; i++) {
+            System.out.println(weigths[0] + " , " + weigths[1] + " , " + weigths[2]);
+            System.out.println(weigths[3] + " , " + weigths[4] + " , " + weigths[5]);
+            System.out.println(weigths[6] + " , " + weigths[7] + " , " + weigths[8] + " , " + weigths[9]);
         //Задание №3
-        System.out.println(weigh[2]+ " , " + weigh[1]+ " , " + weigh[0]);
-        System.out.println(weigh[5]+ " , " + weigh[4]+ " , " + weigh[3]);
-        System.out.println(weigh[10]+ " , " + weigh[9]+ " , " + weigh[8] + " , " + weigh[7]+ " , " + weigh[6]);
+            System.out.println(weigths[2] + " , " + weigths[1] + " , " + weigths[0]);
+            System.out.println(weigths[5] + " , " + weigths[4] + " , " + weigths[3]);
+            System.out.println(weigths[9] + " , " + weigths[8] + " , " + weigths[7] + " , " + weigths[6]);
+        }
         //Задание №4
         int [] number = new int[10];
         number[0] = 1+1;
         number[1] = 2;
         number[2] = 3+1;
+        for (int i = 1; i >= 1; i--)
         System.out.println(number[0]+ " , " + number[1]+ " , " + number[2]);
     }
     }
