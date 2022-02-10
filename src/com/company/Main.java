02f41b5a01e6b81839ec0f9100a53186a9e388bd
Semec <Semechkov98@gmail.com>
2022-02-10 10:54:09 +0300

    ДЗ Сдано, пересдача #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,22 +22,22 @@ public class Main {
         }
         //Задание №2
         float [] weigths = {1,2,3,1.57f,7.654f,9.986f,1.233f, 2.33f, 4.111f,33f};
-        for (int i = 0; i < 1; i++) {
-            System.out.println(weigths[0] + " , " + weigths[1] + " , " + weigths[2]);
-            System.out.println(weigths[3] + " , " + weigths[4] + " , " + weigths[5]);
-            System.out.println(weigths[6] + " , " + weigths[7] + " , " + weigths[8] + " , " + weigths[9]);
+        for (int i = 0; i < weigths.length; i++) {
+            System.out.print(weigths[0] + " , " + weigths[1] + " , " + weigths[2]);
+            System.out.print(weigths[3] + " , " + weigths[4] + " , " + weigths[5]);
+            System.out.print(weigths[6] + " , " + weigths[7] + " , " + weigths[8] + " , " + weigths[9]);
         //Задание №3
-            System.out.println(weigths[2] + " , " + weigths[1] + " , " + weigths[0]);
-            System.out.println(weigths[5] + " , " + weigths[4] + " , " + weigths[3]);
-            System.out.println(weigths[9] + " , " + weigths[8] + " , " + weigths[7] + " , " + weigths[6]);
+            System.out.print(weigths[2] + " , " + weigths[1] + " , " + weigths[0]);
+            System.out.print(weigths[5] + " , " + weigths[4] + " , " + weigths[3]);
+            System.out.print(weigths[9] + " , " + weigths[8] + " , " + weigths[7] + " , " + weigths[6]);
         }
         //Задание №4
         int [] number = new int[10];
         number[0] = 1+1;
         number[1] = 2;
         number[2] = 3+1;
-        for (int i = 1; i >= 1; i--)
-        System.out.println(number[0]+ " , " + number[1]+ " , " + number[2]);
+        for (int i = 1; i <= number.length; i++)
+        System.out.print(number[0]+ " , " + number[1]+ " , " + number[2]);
     }
     }
 
